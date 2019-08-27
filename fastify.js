01aa0e50038f456cd7d434fc02b543dd73eefc1c
Oswaldo Acauan <oswaldoacauan@users.noreply.github.com>
2019-08-27 09:50:55 +0200

    Pretty logs

--- fastify.js
+++ fastify.js
@@ -2,10 +2,14 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const ENV = process.env.NODE_ENV || 'dev';
+
 // FASTIFY
 // ===============
 const fastify = require("fastify")({
-  logger: true
+  logger: {
+    prettyPrint: ENV !== 'dev' ? false : { colorize: true },
+  },
 });
 
 fastify.register(require("fastify-sensible"));
@@ -21,11 +25,11 @@ fastify.post("/error", async (request, reply) => {
 fastify.post("/async", async (request, reply) => {
   reply.code(200).send("OK");
   await sleep(2000);
-  console.info("--- SLEEP AFTER 2S ---");
+  request.log.info("--- SLEEP AFTER 2S ---");
   await sleep(5000);
-  console.info("--- SLEEP AFTER 5S ---");
+  request.log.info("--- SLEEP AFTER 5S ---");
   await sleep(10000);
-  console.info("--- SLEEP AFTER 10S ---");
+  request.log.info("--- SLEEP AFTER 10S ---");
 });
 
 fastify.post("/async/error", async (request, reply) => {
@@ -34,10 +38,18 @@ fastify.post("/async/error", async (request, reply) => {
     await sleep(3000);
     throw "--- SLEEP ERROR AFTER 3S ---"
   } catch (error) {
-    console.error(error);
+    request.log.error(error);
   }
 });
 
-fastify.listen(3000, (err, address) => {
-  if (err) throw err;
-});
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start();
